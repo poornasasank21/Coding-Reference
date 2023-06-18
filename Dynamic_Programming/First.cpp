9116897a67656577625a5f01481090a4776c2050
POORNA SASANK VANDANAPU <103208320+poornasasank21@users.noreply.github.com>
2023-06-18 22:56:18 +0530

    v0.2

---
+++ Dynamic_Programming/First.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define ld long double
+using namespace std;
+
+int main(void) {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+
+    int t; cin >> t;
+    while(t--) {
+
+    }
+
+    return 0;
+}
